d2f901e520729fb46abff668d4650f6c3b4f73c5
danielaSantaCruz <daniela2010.1d@gmail.com>
2021-03-11 23:43:05 -0400

    Add functionality to IndicatorLight class

--- src/main/java/org/fundacionjala/prog101CoffeeMaker/coffeeMaker/IndicatorLight.java
+++ src/main/java/org/fundacionjala/prog101CoffeeMaker/coffeeMaker/IndicatorLight.java
@@ -1,11 +1,21 @@
 package org.fundacionjala.prog101CoffeeMaker.coffeeMaker;
 
 public class IndicatorLight {
-    public boolean light;
-    public void turnOn(){
+    private boolean light;
 
+    public void turnOn() {
+        light = true;
     }
-    public void turnOff(){
 
+    public void turnOff() {
+        light = false;
+    }
+
+    public boolean getLight() {
+        return light;
+    }
+
+    public void setLight(boolean newLight) {
+        this.light = newLight;
     }
 }
