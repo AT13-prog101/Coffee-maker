c1c5a9a9545526ed55fca693b5d34a39e0fd8eaa
danielaSantaCruz <daniela2010.1d@gmail.com>
2021-03-12 00:28:14 -0400

    Add functionlity to Outputs class with commented method

--- src/main/java/org/fundacionjala/prog101CoffeeMaker/coffeeMaker/Outputs.java
+++ src/main/java/org/fundacionjala/prog101CoffeeMaker/coffeeMaker/Outputs.java
@@ -3,6 +3,10 @@ package org.fundacionjala.prog101CoffeeMaker.coffeeMaker;
 public class Outputs {
     private String response;
 
+    /**
+     * Print method
+     * @param message
+     */
     public void print(String message) {
         System.out.println(message);
     }
