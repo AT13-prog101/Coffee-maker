580fe02b4b7da84b08a5140eb1c1eab25b7ec408
danielaSantaCruz <daniela2010.1d@gmail.com>
2021-03-11 23:38:11 -0400

    Add functionality to StartButton class

--- src/main/java/org/fundacionjala/prog101CoffeeMaker/coffeeMaker/StartButton.java
+++ src/main/java/org/fundacionjala/prog101CoffeeMaker/coffeeMaker/StartButton.java
@@ -1,8 +1,18 @@
 package org.fundacionjala.prog101CoffeeMaker.coffeeMaker;
 
 public class StartButton {
-    public boolean init;
-    public boolean isPressed(){
-        return false;
+    private boolean init;
+
+    public boolean isPressed() {
+        init = true;
+        return init;
+    }
+
+    public boolean getInit() {
+        return init;
+    }
+
+    public void setInit(boolean newInit) {
+        this.init = newInit;
     }
 }
