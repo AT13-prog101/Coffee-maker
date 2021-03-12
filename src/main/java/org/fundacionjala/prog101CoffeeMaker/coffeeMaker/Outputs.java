490d7a620a9e9ac2a00fe2df111c999990dae507
danielaSantaCruz <daniela2010.1d@gmail.com>
2021-03-12 00:25:38 -0400

    Add functionlity to Outputs class

--- src/main/java/org/fundacionjala/prog101CoffeeMaker/coffeeMaker/Outputs.java
+++ src/main/java/org/fundacionjala/prog101CoffeeMaker/coffeeMaker/Outputs.java
@@ -1,8 +1,9 @@
 package org.fundacionjala.prog101CoffeeMaker.coffeeMaker;
 
 public class Outputs {
-    public String response;
-    public void print(String message){
+    private String response;
+
+    public void print(String message) {
         System.out.println(message);
     }
 }
