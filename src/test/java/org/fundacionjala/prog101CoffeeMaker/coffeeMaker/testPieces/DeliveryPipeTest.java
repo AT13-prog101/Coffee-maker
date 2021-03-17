00e39fea7f0298aba8033ca972daa3d532a6e6b8
danielaSantaCruz <daniela2010.1d@gmail.com>
2021-03-17 01:01:48 -0400

    Add unit test for DeliveryTest class

--- src/test/java/org/fundacionjala/prog101CoffeeMaker/coffeeMaker/testPieces/DeliveryPipeTest.java
+++ src/test/java/org/fundacionjala/prog101CoffeeMaker/coffeeMaker/testPieces/DeliveryPipeTest.java
@@ -8,5 +8,19 @@ public class DeliveryPipeTest {
 
     @Test
     public void getDeliveryWarmWater() {
+        DeliveryPipe deliveryPipe = new DeliveryPipe();
+        boolean expected = true;
+        boolean actual = deliveryPipe.getDeliveryWarmWater();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setDeliveryWarmWater() {
+        DeliveryPipe deliveryPipe = new DeliveryPipe();
+        boolean newState = false;
+        boolean expected = false;
+        deliveryPipe.setDeliveryWarmWater(newState);
+        boolean actual = deliveryPipe.getDeliveryWarmWater();
+        assertEquals(expected, actual);
     }
 }
