b212ddfce78915b40cc5e372ac6413498213a9c5
danielaSantaCruz <daniela2010.1d@gmail.com>
2021-03-17 00:52:08 -0400

    Add unit test for PlateSensor and Pot classes

--- src/test/java/org/fundacionjala/prog101CoffeeMaker/coffeeMaker/testPot/PotTest.java
+++ src/test/java/org/fundacionjala/prog101CoffeeMaker/coffeeMaker/testPot/PotTest.java
@@ -7,22 +7,46 @@ import static org.junit.Assert.*;
 public class PotTest {
 
     @Test
-    public void containLiquid() {
+    public void containLiquid_False() {
+        Pot pot = new Pot();
+        boolean expected = false;
+        boolean actual = pot.containLiquid();
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void getIsInPlace() {
+    public void getIsInPlace_IsInPlace() {
+        Pot pot = new Pot();
+        boolean expected = false;
+        boolean actual = pot.getIsInPlace();
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void setIsInPlace() {
+    public void setIsInPlace_NewStateTrue_True() {
+        Pot pot = new Pot();
+        boolean newState = true;
+        boolean expected = true;
+        pot.setIsInPlace(newState);
+        boolean actual = pot.getIsInPlace();
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void getAmountOfCups() {
+    public void getAmountOfCups_AmountOfCups() {
+        Pot pot = new Pot();
+        int expected = 0;
+        int actual = pot.getAmountOfCups();
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void setAmountOfCups() {
+    public void setAmountOfCups_NewAmountFive_Five() {
+        Pot pot = new Pot();
+        int newAmount = 5;
+        int expected = 5;
+        pot.setAmountOfCups(newAmount);
+        int actual = pot.getAmountOfCups();
+        assertEquals(expected, actual);
     }
 }
