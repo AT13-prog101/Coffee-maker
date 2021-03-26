f13e26725afe14b85851b0879f9e6edf49c3f4c6
danielaSantaCruz <daniela2010.1d@gmail.com>
2021-03-26 03:31:28 -0400

    Add all videos for coffee maker

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -13,24 +13,58 @@ public class Controller implements Initializable{
 
     @FXML
     private MediaView mv;
-
     private MediaPlayer mediaPlayer;
     private File file;
     private Media media;
 
-
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        file = new File("C:\\Jala\\progra101\\edson\\water.mp4");
-        media = new Media(file.toURI().toString());
-        mediaPlayer = new MediaPlayer(media);
-        mv.setMediaPlayer(mediaPlayer);
+
     }
 
     @FXML
     private void play_Video() {
+        file = new File("C:\\Jala\\progra101\\edson\\startButton.mp4");
+        media = new Media(file.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mv.setMediaPlayer(mediaPlayer);
         mediaPlayer.play();
+    }
 
+    @FXML
+    private void playWater() {
+        file = new File("C:\\Jala\\progra101\\edson\\water.mp4");
+        media = new Media(file.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mv.setMediaPlayer(mediaPlayer);
+        mediaPlayer.play();
+    }
+
+    @FXML
+    private void playCoffee() {
+        file = new File("C:\\Jala\\progra101\\edson\\beansCoffee.mp4");
+        media = new Media(file.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mv.setMediaPlayer(mediaPlayer);
+        mediaPlayer.play();
+    }
+
+    @FXML
+    private void playPutPot() {
+        file = new File("C:\\Jala\\progra101\\edson\\loadPot.mp4");
+        media = new Media(file.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mv.setMediaPlayer(mediaPlayer);
+        mediaPlayer.play();
+    }
+
+    @FXML
+    private void playRemovePot() {
+        file = new File("C:\\Jala\\progra101\\edson\\removePot.mp4");
+        media = new Media(file.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mv.setMediaPlayer(mediaPlayer);
+        mediaPlayer.play();
     }
 
 }
